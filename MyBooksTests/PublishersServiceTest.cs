72706507cd69bdc0588305a039d79b112a58b7eb
Dimitrii Vinogradov <siteddv@gmail.com>
2021-10-24 15:33:48 +0600

    set up in-memory db

--- MyBooksTests/PublishersServiceTest.cs
+++ MyBooksTests/PublishersServiceTest.cs
@@ -1,14 +1,134 @@
+using Microsoft.EntityFrameworkCore;
+using MyBooksLibrary.Data;
+using MyBooksLibrary.Data.Models;
 using NUnit.Framework;
+using System;
+using System.Collections.Generic;
 
 namespace MyBooksTests
 {
     public class PublishersServiceTest
     {
-        [SetUp]
+        private static DbContextOptions<AppDbContext> dbContextOptions = new DbContextOptionsBuilder<AppDbContext>()
+            .UseInMemoryDatabase("PublishersDbTest")
+            .Options;
+
+        private AppDbContext _context;
+
+        [OneTimeSetUp]
         public void Setup()
         {
+            _context = new AppDbContext(dbContextOptions);
+            _context.Database.EnsureCreated();
+
+            SeedDatabase();
         }
 
-        
+        [OneTimeTearDown]
+        public void CleanUp()
+        {
+            _context.Database.EnsureDeleted();
+        }
+
+        private void SeedDatabase()
+        {
+            var publishers = new List<Publisher>
+            {
+                    new Publisher() {
+                        Id = 1,
+                        Name = "Publisher 1"
+                    },
+                    new Publisher() {
+                        Id = 2,
+                        Name = "Publisher 2"
+                    },
+                    new Publisher() {
+                        Id = 3,
+                        Name = "Publisher 3"
+                    },
+                    new Publisher() {
+                        Id = 4,
+                        Name = "Publisher 4"
+                    },
+                    new Publisher() {
+                        Id = 5,
+                        Name = "Publisher 5"
+                    },
+                    new Publisher() {
+                        Id = 6,
+                        Name = "Publisher 6"
+                    },
+            };
+            _context.Publishers.AddRange(publishers);
+
+            var authors = new List<Author>()
+            {
+                new Author()
+                {
+                    Id = 1,
+                    FullName = "Author 1"
+                },
+                new Author()
+                {
+                    Id = 2,
+                    FullName = "Author 2"
+                }
+            };
+            _context.Authors.AddRange(authors);
+
+
+            var books = new List<Book>()
+            {
+                new Book()
+                {
+                    Id = 1,
+                    Title = "Book 1 Title",
+                    Description = "Book 1 Description",
+                    IsRead = false,
+                    Genre = "Genre",
+                    CoverUrl = "https://...",
+                    DateAdded = DateTime.Now.AddDays(-10),
+                    PublisherId = 1
+                },
+                new Book()
+                {
+                    Id = 2,
+                    Title = "Book 2 Title",
+                    Description = "Book 2 Description",
+                    IsRead = false,
+                    Genre = "Genre",
+                    CoverUrl = "https://...",
+                    DateAdded = DateTime.Now.AddDays(-10),
+                    PublisherId = 1
+                }
+            };
+            _context.Books.AddRange(books);
+
+            var books_authors = new List<Book_Author>()
+            {
+                new Book_Author()
+                {
+                    Id = 1,
+                    BookId = 1,
+                    AuthorId = 1
+                },
+                new Book_Author()
+                {
+                    Id = 2,
+                    BookId = 1,
+                    AuthorId = 2
+                },
+                new Book_Author()
+                {
+                    Id = 3,
+                    BookId = 2,
+                    AuthorId = 2
+                },
+            };
+            _context.Book_Authors.AddRange(books_authors);
+
+
+            _context.SaveChanges();
+        }
     }
 }
