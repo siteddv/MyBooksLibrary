d8582ad114698b508c3052cd7c9b08f852e1b001
Dimitrii Vinogradov <siteddv@gmail.com>
2021-10-24 15:08:50 +0600

    set up tests project

---
+++ MyBooksTests/PublishersServiceTest.cs
@@ -0,0 +1,14 @@
+using NUnit.Framework;
+
+namespace MyBooksTests
+{
+    public class PublishersServiceTest
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        
+    }
+}
